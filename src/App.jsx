1a97635a0f71c1cbf195e3d4ef15202b4d1fcad0
Sunny Hundal <amandip.h@gmail.com>
2024-01-19 21:07:21 -0800

    Add header to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingRoute from "./routes/LandingRoute";
+import Header from "./components/header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<LandingRoute />} />
       </Routes>
