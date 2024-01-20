f96f5571e9450145dd39721a3f98ae91f9d20c61
Sunny Hundal <amandip.h@gmail.com>
2024-01-19 20:46:38 -0800

    Added sign up box

--- src/routes/LandingRoute.jsx
+++ src/routes/LandingRoute.jsx
@@ -17,6 +17,50 @@ export default function LandingRoute() {
             The easiest way to split your budget with your friends and family
           </p>
         </div>
+        <div className="flex flex-col items-center justify-center w-1/2 pl-10">
+          <div className="flex flex-col items-center justify-center w-full p-4 bg-white rounded-lg shadow-lg">
+            <h1 className="text-2xl font-bold text-center text-gray-800">
+              Sign up now!
+            </h1>
+            <p className="mt-4 text-xl text-center text-gray-600">
+              Start managing your budget today!
+            </p>
+            <div className="flex flex-row items-center justify-center w-full mt-8">
+            </div>
+            <div className="flex flex-row items-center justify-center w-full mt-4">
+              <div className="flex flex-col items-center justify-center w-full p-4 bg-white rounded-lg shadow-lg">
+                <h1 className="text-2xl font-bold text-center text-gray-800">
+                  Sign up with email
+                </h1>
+                <form className="flex flex-col items-center justify-center w-full mt-4">
+                  <input
+                    className="w-full p-2 px-4 text-xl text-gray-800 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                    type="text"
+                    placeholder="Name"
+                  />
+                  <input
+                    className="w-full p-2 px-4 mt-4 text-xl text-gray-800 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                    type="text"
+                    placeholder="Email"
+                  />
+                  <input
+                    className="w-full p-2 px-4 mt-4 text-xl text-gray-800 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                    type="password"
+                    placeholder="Password"
+                  />
+                  <input
+                    className="w-full p-2 px-4 mt-4 text-xl text-gray-800 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                    type="password"
+                    placeholder="Confirm password"
+                  />
+                  <button className="w-full p-2 px-4 mt-4 text-xl font-bold text-white bg-blue-500 rounded-lg focus:outline-none">
+                    Sign up
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* Features */}
@@ -41,20 +85,6 @@ export default function LandingRoute() {
               <p className="mt-4 text-xl text-center text-gray-600">
                 Track your expenses and easily visualize your spending.
               </p>
-
-              <div className="flex flex-row items-center justify-center w-full mt-16">
-                <div className="flex flex-col items-center justify-center">
-                  <div className="flex flex-row items-center justify-center w-16 h-16 bg-red-500 rounded-full">
-                    <i className="text-3xl text-white fas fa-users"></i>
-                  </div>
-                  <h1 className="mt-4 text-2xl font-bold text-center text-gray-800">
-                    Organize your budget
-                  </h1>
-                  <p className="mt-4 text-xl text-center text-gray-600">
-                    Split your budget with your friends and family. It's easy!
-                  </p>
-                </div>
-              </div>
             </div>
           </div>
         </div>
