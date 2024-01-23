0a4ebf4605b0e694c4fa145a9cbcae086ec54135
Sunny Hundal <amandip.h@gmail.com>
2024-01-22 23:44:18 -0800

    Add route to app and start dashboard framework

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingRoute from "./routes/LandingRoute";
+<<<<<<< Updated upstream
+=======
+import Header from "./components/header";
+import Dashboard from "./routes/dashboard";
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<h1>Not Found</h1>} />
         <Route path="/" element={<LandingRoute />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    // create a div that will generate a white box for dashboard components
+    <div className="dashboard">
+      <div className="dashboard__content">
+        <h1>Dashboard</h1>
+      </div>
+    </div>
+  );
+}
