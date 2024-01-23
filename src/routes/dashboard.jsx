166c583f9bf13d02f52eb1361dcb50ce725cff27
Sunny Hundal <amandip.h@gmail.com>
2024-01-22 23:54:10 -0800

    TailwindCSS added to start

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -3,9 +3,16 @@ import React from "react";
 export default function Dashboard() {
   return (
     // create a div that will generate a white box for dashboard components
-    <div className="dashboard">
-      <div className="dashboard__content">
-        <h1>Dashboard</h1>
+    <div className="flex flex-row">
+      <div className="flex  border-cyan-700 border">
+        <div className="flex border-red-700 border">
+          <div className="flex border justify-start border-slate-800">
+            <h1>Left zone</h1>
+            <div className="flex border justify-end border-slate-800">
+              <h1>right zone</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
